2ee83d88eb00b0c1dde450a4451c6c991b7d8891
aherman <arthur.herman@etu.u-bordeaux.fr>
2019-11-05 16:51:06 +0100

    Ajout de l'affichage de la page de profil

---
+++ profil.js
@@ -0,0 +1,21 @@
+/*function openForm(){
+    document.getElementById("myForm").style.display = "block";
+}
+function closeForm(){
+    document.getElementById("myForm").style.display = "none";
+}
+
+/*function fillTables(){
+    let projectsTable = document.getElementById("projectsList");
+    let rows = projectsTable.getElementsByTagName("tr").length;
+    while(rows<5){
+        let row = document.createElement("tr");
+        let cell = document.createElement("td");
+        row.appendChild(cell);
+        projectsTable.appendChild(row);
+        rows++;
+    }
+}
+document.onload = function(){fillTables()};
+document.getElementById("openForm").addEventListener("click", openForm);
+document.getElementById("closeForm").addEventListener("click", closeForm);*/
