536edc5e8b8ec0f51feef5fa22783478c784c1c8
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-11-07 15:17:40 +0100

    Ajout de l'affichage de la page de projet

---
+++ projet.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Mon profil - GoProject</title>
+    <link href="style2.css" rel="stylesheet">
+    <script src="profil.js" defer></script>
+</head>
+
+
+
+<body>
+<div class = "menuBar">
+    <span id="title">GoProject</span>
+    <div class="menuBar-right">
+        <a class = "disconnect">Se déconnecter</a>
+    </div>
+</div>
+<div class="pageName">
+    <p>Nom du projet</p>
+</div>
+
+<div class = "container">
+    <table class="a" id="issuesList">
+        <tr>
+            <td class="listTitle">Les issues</td>
+        </tr>
+    </table>
+</div>
+
+<div class = "container">
+    <table class="b" id="TasksList">
+        <tr>
+            <td class="listTitle">Les tâches</td>
+        </tr>
+    </table>
+</div>
+
+<div class = "container">
+    <table class="c" id="releasesList">
+        <tr>
+            <td class="listTitle">Les releases</td>
+        </tr>
+    </table>
+</div>
+
+<div class = "container">
+    <table class="d" id="testsList">
+        <tr>
+            <td class="listTitle">Les test</td>
+        </tr>
+    </table>
+</div>
+
+<div class = "container">
+    <table class="e" id="docsList">
+        <tr>
+            <td class="listTitle">La doc</td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
