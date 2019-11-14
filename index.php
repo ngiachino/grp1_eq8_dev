c7eeffecd3d6bbe52d4630d806e8303e3af2933d
aherman <arthur.herman@etu.u-bordeaux.fr>
2019-11-14 11:23:43 +0100

    Requête SQL connexion

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ if ($conn->connect_error) {
     <?php include './defaultHead.php'; ?>
     <title>Mon profil - GoProject</title>
     <link href="index.css" rel="stylesheet">
-    <script src="profil.js" defer></script>
 </head>
 
 <body>
@@ -40,7 +39,7 @@ if ($conn->connect_error) {
             <input type="password" name="pswdCo" id="pswdCo">
         </div>
         <div>
-            <button type="submit" form="submitCo">Se connecter</button>
+            <input type="submit" name="submitCo" value = "Se connecter">
         </div>
     </div>
 
@@ -49,19 +48,19 @@ if ($conn->connect_error) {
         <h2>Inscription</h2>
         <div>
             <label for="pseudoInsc">Pseudo</label>
-            <input type="text" required name="pseudoInsc" id="pseudoInsc">
+            <input type="text" name="pseudoInsc" id="pseudoInsc">
         </div>
         <div>
             <label for="mailInsc">Email</label>
-            <input type="email" required name="mailInsc" id="mailInsc">
+            <input type="email" name="mailInsc" id="mailInsc">
         </div>
         <div>
             <label for="pswdInsc">Mot de passe</label>
-            <input type="password" required name="pswdInsc" id="pswdInsc">
+            <input type="password" name="pswdInsc" id="pswdInsc">
         </div>
         <div>
             <label for="pswdConfirmInsc">Confirmer mot de passe</label>
-            <input type="password" required name="pswdConfirmInsc" id="pswdConfirmInsc">
+            <input type="password" name="pswdConfirmInsc" id="pswdConfirmInsc">
         </div>
         <div>
             <input type="submit" name="submitInsc" value="S'inscrire">
@@ -76,35 +75,68 @@ if ($conn->connect_error) {
         $pswdInsc = $_POST['pswdInsc'];
         $pswdConfirmInsc = $_POST['pswdConfirmInsc'];
 
-        //test que le mail n'est pas déjà utilisé
-        $sqlTest1 = "SELECT ID_USER FROM utilisateur WHERE MAIL_USER = '$mailInsc'";
-        $result1 = $conn->query($sqlTest1);
-
-        //test que le pseudo n'est pas déjà utilisé
-        $sqlTest2 = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = '$pseudoInsc'";
-        $result2 = $conn->query($sqlTest2);
-
-
+        //test que tous les champs sont remplis
+        if(empty($pseudoInsc) || empty($mailInsc) || empty($pswdInsc) || empty($pswdConfirmInsc)){
+            echo("Vous devez remplir tous les champs");
+        }
         //test que les deux mots de passe sont identiques
-        if($pswdInsc != $pswdConfirmInsc){
+        else if($pswdInsc != $pswdConfirmInsc){
             echo("Les mots de passe ne sont pas identiques");
         }
-        else if($result1->num_rows > 0){
-            echo("Ce mail est déjà associé à un compte");
-        }
-        else if($result2->num_rows > 0){
-            echo("Ce pseudo est déjà associé à un compte");
-        }
         else{
-            $sql = "INSERT INTO utilisateur (NOM_USER, PASSWORD_USER, MAIL_USER)
-            VALUES ('$pseudoInsc','$pswdInsc','$mailInsc')";
-            if ($conn->query($sql) === FALSE) {
-                echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+
+            //test que le mail n'est pas déjà utilisé
+            $sqlTest1 = "SELECT ID_USER FROM utilisateur WHERE MAIL_USER = '$mailInsc'";
+            $result1 = $conn->query($sqlTest1);
+
+            //test que le pseudo n'est pas déjà utilisé
+            $sqlTest2 = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = '$pseudoInsc'";
+            $result2 = $conn->query($sqlTest2);
+
+            if(empty($pseudoInsc) || empty($mailInsc) || empty($pswdInsc) || empty($pswdConfirmInsc)){
+                echo("Vous devez remplir tous les champs");
+            }
+            //test que les deux mots de passe sont identiques
+            else if($pswdInsc != $pswdConfirmInsc){
+                echo("Les mots de passe ne sont pas identiques");
+            }
+            else if($result1->num_rows > 0){
+                echo("Ce mail est déjà associé à un compte");
+            }
+            else if($result2->num_rows > 0){
+                echo("Ce pseudo est déjà associé à un compte");
+            }
+            else{
+                $sql = "INSERT INTO utilisateur (NOM_USER, PASSWORD_USER, MAIL_USER)
+                VALUES ('$pseudoInsc','$pswdInsc','$mailInsc')";
+                if ($conn->query($sql) === FALSE) {
+                    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+                }
+                else{
+                    echo("Votre compte a bien été crée");
+                }
             }
         }
-        
     }
+    else if(isset($_POST['submitCo'])){
+        $nameCo = $_POST['nameCo'];
+        $pswdCo = $_POST['pswdCo'];
 
+        //test que le mail n'est pas déjà utilisé
+        $sql = "SELECT ID_USER FROM utilisateur WHERE (MAIL_USER = '$nameCo' OR NOM_USER = '$nameCo') AND PASSWORD_USER = '$pswdCo'";
+        $result = $conn->query($sql);
+        if($result === FALSE){
+            echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+        }
+
+        else if($result->num_rows == 0){
+            echo("Ce compte n'existe pas");
+        }
+        else{
+            echo "Vous êtes connecté"; 
+        }
+    }
+  ?>
 
 </body>
 </html>
