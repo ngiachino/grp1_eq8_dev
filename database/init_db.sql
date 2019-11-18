fd66d935614f10064402967c3250b20a224a6848
aherman <arthur.herman@etu.u-bordeaux.fr>
2019-11-18 10:22:36 +0100

    Mise à jour init_db

--- database/init_db.sql
+++ database/init_db.sql
@@ -1,173 +1,226 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.2deb1ubuntu0.2
--- http://www.phpmyadmin.net
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
 --
--- Serveur: localhost
--- Généré le : Jeu 07 Novembre 2019 à 13:15
--- Version du serveur: 5.0.75
--- Version de PHP: 5.2.6-3ubuntu4.6
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 18 nov. 2019 à 10:21
+-- Version du serveur :  10.4.8-MariaDB
+-- Version de PHP :  7.3.11
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données: `aouldamara`
+-- Base de données :  `cdp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Documentation`
+-- Structure de la table `documentation`
 --
 
-CREATE TABLE IF NOT EXISTS `Documentation` (
-  `ID_DOCUMENTATION` int(50) unsigned NOT NULL,
+CREATE TABLE `documentation` (
+  `ID_DOCUMENTATION` int(50) UNSIGNED NOT NULL,
   `TITRE` varchar(100) NOT NULL,
-  `DESCRIPTION` varchar(500) NOT NULL,
-  PRIMARY KEY  (`ID_DOCUMENTATION`)
+  `DESCRIPTION` varchar(500) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='La table des Documentations';
 
---
--- Contenu de la table `Documentation`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Issue`
+-- Structure de la table `issue`
 --
 
-CREATE TABLE IF NOT EXISTS `Issue` (
-  `ID_USER_STORY` int(50) unsigned NOT NULL COMMENT '"Identifiant de la user story"',
+CREATE TABLE `issue` (
+  `ID_USER_STORY` int(50) UNSIGNED NOT NULL COMMENT '"Identifiant de la user story"',
   `PRIORITE` varchar(10) NOT NULL COMMENT '"Les valeurs de la priorité sont Basse, Moyenne, Haute "',
   `DIFFICULTE` varchar(10) NOT NULL COMMENT '"Les valeurs de la difficulté sont Basse, Moyenne, Haute "',
-  `DESCRIPTION` varchar(500) NOT NULL,
-  PRIMARY KEY  (`ID_USER_STORY`)
+  `DESCRIPTION` varchar(500) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='La table des Issues';
 
---
--- Contenu de la table `Issue`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Membre`
+-- Structure de la table `membre`
 --
 
-CREATE TABLE IF NOT EXISTS `Membre` (
-  `ID_MEMBRE` int(50) unsigned NOT NULL,
-  `ID_PROJET` int(50) unsigned NOT NULL,
+CREATE TABLE `membre` (
+  `ID_MEMBRE` int(50) UNSIGNED NOT NULL,
+  `ID_PROJET` int(50) UNSIGNED NOT NULL,
   `NOM_MEMBRE` varchar(50) NOT NULL,
-  `ID_SPRINT` int(50) unsigned NOT NULL,
-  `ID_TACHE` int(50) unsigned NOT NULL,
-  PRIMARY KEY  (`ID_MEMBRE`,`ID_PROJET`,`ID_SPRINT`,`ID_TACHE`)
+  `ID_SPRINT` int(50) UNSIGNED NOT NULL,
+  `ID_TACHE` int(50) UNSIGNED NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `Membre`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Projet`
+-- Structure de la table `projet`
 --
 
-CREATE TABLE IF NOT EXISTS `Projet` (
-  `ID_PROJET` int(50) unsigned NOT NULL COMMENT '"Identifiant du projet"',
-  `NOM_PROJET` varchar(20) character set utf8 collate utf8_bin NOT NULL COMMENT '"Intitulé du projet"',
-  `ID_MANAGER` int(50) unsigned NOT NULL COMMENT '"Identifiant du Créateur du projet"',
-  `DESCRIPTION` text character set utf8 collate utf8_bin NOT NULL COMMENT '" Description du projet"',
-  PRIMARY KEY  (`ID_PROJET`,`ID_MANAGER`),
-  UNIQUE KEY `nom_projet` (`NOM_PROJET`)
+CREATE TABLE `projet` (
+  `ID_PROJET` int(50) UNSIGNED NOT NULL COMMENT '"Identifiant du projet"',
+  `NOM_PROJET` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '"Intitulé du projet"',
+  `ID_MANAGER` int(50) UNSIGNED NOT NULL COMMENT '"Identifiant du Créateur du projet"',
+  `DESCRIPTION` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '" Description du projet"'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='L''entité Projet';
 
---
--- Contenu de la table `Projet`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Release`
+-- Structure de la table `release`
 --
 
-CREATE TABLE IF NOT EXISTS `Release` (
+CREATE TABLE `release` (
   `ID_RELEASE` int(50) NOT NULL,
   `ID_PROJET` int(50) NOT NULL,
   `DESCRIPTION` varchar(500) NOT NULL,
   `DATE_RELEASE` date NOT NULL,
-  `URL_DOCKER` varchar(500) NOT NULL,
-  PRIMARY KEY  (`ID_RELEASE`)
+  `URL_DOCKER` varchar(500) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='La table des Releases';
 
---
--- Contenu de la table `Release`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Sprint`
+-- Structure de la table `sprint`
 --
 
-CREATE TABLE IF NOT EXISTS `Sprint` (
-  `ID_SPRINT` int(50) unsigned NOT NULL COMMENT '"identifiant et numéro du sprint"',
-  `ID_PROJET` int(50) unsigned NOT NULL COMMENT '"identifiant du projet auquel appartient le sprint"',
+CREATE TABLE `sprint` (
+  `ID_SPRINT` int(50) UNSIGNED NOT NULL COMMENT '"identifiant et numéro du sprint"',
+  `ID_PROJET` int(50) UNSIGNED NOT NULL COMMENT '"identifiant du projet auquel appartient le sprint"',
   `DATE_DEBUT` date NOT NULL,
-  `DATE_FIN` date NOT NULL,
-  PRIMARY KEY  (`ID_SPRINT`,`ID_PROJET`)
+  `DATE_FIN` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des sprints';
 
---
--- Contenu de la table `Sprint`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Tache`
+-- Structure de la table `tache`
 --
 
-CREATE TABLE IF NOT EXISTS `Tache` (
-  `ID_TACHE` int(50) unsigned NOT NULL COMMENT '"Identifiant de la tâche"',
-  `ID_PROJET` int(50) unsigned NOT NULL COMMENT '"identifiant du projet auquel appartient la tâche"',
-  `ID_SPRINT` int(50) unsigned NOT NULL COMMENT '"Identifiant du sprint auquel appartient la tâche"',
-  `ID_USER_STORY` int(50) unsigned NOT NULL COMMENT '"Identifiant de la USS qui correspond à la tâche"',
+CREATE TABLE `tache` (
+  `ID_TACHE` int(50) UNSIGNED NOT NULL COMMENT '"Identifiant de la tâche"',
+  `ID_PROJET` int(50) UNSIGNED NOT NULL COMMENT '"identifiant du projet auquel appartient la tâche"',
+  `ID_SPRINT` int(50) UNSIGNED NOT NULL COMMENT '"Identifiant du sprint auquel appartient la tâche"',
+  `ID_USER_STORY` int(50) UNSIGNED NOT NULL COMMENT '"Identifiant de la USS qui correspond à la tâche"',
   `DESCRIPTION` int(50) NOT NULL,
   `DUREE_TACHE` float NOT NULL COMMENT '" Durée de la tâche. Ne dépasse pas une journée"',
   `IS_DONE` tinyint(1) NOT NULL COMMENT '"Indique si la tâche a été effectuée"',
-  `IS_CLOSED` tinyint(1) NOT NULL COMMENT '"Indique si la tâche a été cloturée Dans le cas où elle n''a pas été traitée, elle sera transférée vers le nouveau sprint ou abandonnée"',
-  PRIMARY KEY  (`ID_TACHE`,`ID_PROJET`,`ID_SPRINT`,`ID_USER_STORY`)
+  `IS_CLOSED` tinyint(1) NOT NULL COMMENT '"Indique si la tâche a été cloturée Dans le cas où elle n''a pas été traitée, elle sera transférée vers le nouveau sprint ou abandonnée"'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='La table des tâches';
-
---
--- Contenu de la table `Tache`
---
-
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Test`
+-- Structure de la table `test`
 --
 
-CREATE TABLE IF NOT EXISTS `Test` (
-  `ID_TEST` int(50) unsigned NOT NULL,
-  `ID_PROJET` int(50) unsigned NOT NULL,
+CREATE TABLE `test` (
+  `ID_TEST` int(50) UNSIGNED NOT NULL,
+  `ID_PROJET` int(50) UNSIGNED NOT NULL,
   `DATE_DEBUT` date NOT NULL,
   `ETAT` tinyint(1) NOT NULL,
-  `DESCRIPTION` varchar(500) NOT NULL,
-  PRIMARY KEY  (`ID_TEST`)
+  `DESCRIPTION` varchar(500) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='La table des tests';
 
+-- --------------------------------------------------------
+
 --
--- Contenu de la table `Test`
+-- Structure de la table `utilisateur`
 --
 
+CREATE TABLE `utilisateur` (
+  `ID_USER` int(50) UNSIGNED NOT NULL,
+  `NOM_USER` varchar(20) NOT NULL,
+  `PASSWORD_USER` varchar(50) NOT NULL,
+  `MAIL_USER` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `documentation`
+--
+ALTER TABLE `documentation`
+  ADD PRIMARY KEY (`ID_DOCUMENTATION`);
+
+--
+-- Index pour la table `issue`
+--
+ALTER TABLE `issue`
+  ADD PRIMARY KEY (`ID_USER_STORY`);
+
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`ID_MEMBRE`,`ID_PROJET`,`ID_SPRINT`,`ID_TACHE`);
+
+--
+-- Index pour la table `projet`
+--
+ALTER TABLE `projet`
+  ADD PRIMARY KEY (`ID_PROJET`,`ID_MANAGER`);
+
+--
+-- Index pour la table `release`
+--
+ALTER TABLE `release`
+  ADD PRIMARY KEY (`ID_RELEASE`);
+
+--
+-- Index pour la table `sprint`
+--
+ALTER TABLE `sprint`
+  ADD PRIMARY KEY (`ID_SPRINT`,`ID_PROJET`);
+
+--
+-- Index pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`ID_TACHE`,`ID_PROJET`,`ID_SPRINT`,`ID_USER_STORY`);
+
+--
+-- Index pour la table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`ID_TEST`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`ID_USER`),
+  ADD UNIQUE KEY `NOM_USER` (`NOM_USER`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `projet`
+--
+ALTER TABLE `projet`
+  MODIFY `ID_PROJET` int(50) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '"Identifiant du projet"', AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `ID_USER` int(50) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
