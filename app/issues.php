02965a0a834dc9e018270ee0d186ab99baa1ebf2
aherman <arthur.herman@etu.u-bordeaux.fr>
2019-11-18 17:36:08 +0100

    Visuel page issues avec champs pour création

--- app/issues.php
+++ app/issues.php
@@ -1,3 +1,18 @@
+<?php
+
+include '../database/DBconnect.php';
+$conn = connect();
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$query = "SELECT ID_USER_STORY,PRIORITE,DIFFICULTE,DESCRIPTION FROM issue";
+$result = mysqli_query($conn, $query);
+
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -17,12 +32,36 @@
         <a class="disconnect">Se déconnecter</a>
     </div>
 </div>
-
-<div class="supercontainer">
-    <div class="container" id="issues">
-            <h2>Les issues</h2>
-    </div>
-</div>
-
+<h1>Issues</h1>
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Description</th>
+      <th scope="col">Priorité</th>
+      <th scope="col">Difficulté</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php $i = 0;
+    while($row = mysqli_fetch_row($result)){?>
+    <tr>
+      <th scope="row"><?php echo $row[0];?></th>
+      <td><?php echo $row[3];?></td>
+      <td><?php echo $row[1];?></td>
+      <td><?php echo $row[2];?></td>
+      <td><button class="modify" type="button">Modifier</button><button class="supprimer" type="button">Supprimer</button></td>
+    </tr>
+    <?php } ?>
+    <tr>
+        <th scope="row"><input type="number" name="idUS"></th>
+        <td><input type="text" name="description"></td>
+        <td><input type="text" name="priorité"></td>
+        <td><input type="number" name="difficulté"></td>
+        <td><input type="submit" name="submit" value="Créer"></td> 
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
