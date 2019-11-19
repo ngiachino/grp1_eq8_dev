4ad5a7d02d0296727416923bfe4baa0a814ababb
Amel OULD AMARA <ouldamara.amel@gmail.com>
2019-11-20 00:51:50 +0100

    Ajout, Suppression et affectation des tâches

---
+++ app/taskManagement.php
@@ -0,0 +1,48 @@
+<?php
+function addTask($conn,$projectId,$sprintId,$userStoryId)
+{
+    if (isset($_POST['taskDescription'])
+        && isset($_POST['taskDuration']))
+    {
+        $description = $_POST['taskDescription'];
+        $duration = $_POST['taskDuration'];
+
+        $query = "INSERT INTO Tache (
+                             ID_PROJECT,
+                             ID_SPRINT,
+                             ID_USER_STORY,
+                             DESCRIPTION,
+                             DUREE_TACHE,
+                             IS_DONE,
+                             IS_CLOSED       
+                          )
+                          VALUE (
+                           $projectId,
+                           $sprintId,
+                           $userStoryId,
+                           $description,
+                           $duration,
+                           0,
+                           0
+                          );                  
+                  ";
+        $result = mysqli_query($conn,$query);
+
+    }
+}
+
+
+function assignTask($conn, $taskId, $userId)
+{
+    $query = "SELECT ID_SPRINT  FROM TACHE WHERE ID_TACHE = '$taskId'";
+    $sprintId = mysqli_query($conn, $query);
+}
+function deleteTask($conn)
+{
+    if (isset($_POST['taskId'])) {
+        $taskId = $_POST['taskId'];
+        $query = "DELETE FROM Tache WHERE ID_TACHE = '$taskId'";
+        $result = mysqli_query($conn,$query);
+    }
+}
+?>
