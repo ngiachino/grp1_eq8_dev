5db9687093b1513b51d2673330691f6f96106f66
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-11-19 17:38:28 +0100

    Mise à jour de la page de profil

--- app/profil.php
+++ app/profil.php
@@ -1,5 +1,5 @@
 <?php
-
+include './projectCreation.php';
 include '../database/DBconnect.php';
 $conn = connect();
 // Check connection
@@ -16,6 +16,8 @@ if($_SESSION['userName'] == null || $_SESSION['userID'] == null){
 $userName = $_SESSION['userName'];
 $userID = $_SESSION['userID'];
 
+$message = addProject($conn, $userID, $userName);
+
 //Selection liste de projets
 $query = "SELECT NOM_PROJET,NOM_USER FROM projet
 JOIN utilisateur ON projet.ID_MANAGER = utilisateur.ID_USER
@@ -24,6 +26,7 @@ WHERE ID_MEMBRE = '$userID'";
 $result1 = mysqli_query($conn,$query);
 ?>
 
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -31,135 +34,114 @@ $result1 = mysqli_query($conn,$query);
     <?php include './defaultHead.php'; ?>
     <title>Mon profil - GoProject</title>
     <link href="../assets/css/profil.css" rel="stylesheet">
-    <script src="../assets/js/profil.js" defer></script>
+    <!-- <script src="../assets/js/profil.js" defer></script>-->
 </head>
-
 
 
 <body>
 <div class = "menuBar">
     <span id="title">GoProject</span>
     <div class="menuBar-right">
-        <a class = "disconnect" href="./index.php">Se déconnecter</a>
+        <a class="disconnect" href="./index.php">Se déconnecter</a>
     </div>
 </div>
-<div class="pageName">
-    <p>Mon Profil</p>
+
+<h1 class="text-center">Mon Profil</h1>
+
+<div class="container">
+    <div class="row justify-content-aroud">
+        <div class = "col-sm-4">
+            <table class="table table-bordered table-hover" id="projectsList" summary="Table des projets de l'utilisateur">
+                <tr>
+                    <th class="listTitle text-center" id="projectListTitle">Mes projets</th>
+                </tr>
+                <?php $i = 0;
+                while($row1 = mysqli_fetch_row($result1)){?>
+                    <tr>
+                        <td class="tdProject">
+                            <a class="d-inline-block w-100 h-100" href="./projet.php?title=<?php echo $row1[0];?>&owner=<?php echo $row1[1];?>">
+                                <span class="projectTitle"><?php echo $row1[0];?></span>
+                                <br/>
+                                <span class="projectOwner"><?php echo $row1[1];?></span>
+                            </a>
+                        </td>
+                    </tr>
+                    <?php
+                    $i++;
+                }
+                while ($i<4){
+                    echo '<tr><td></td></tr>';
+                    $i++;
+                }
+                ?>
+            </table>
+        </div>
+
+        <div class="col-sm-4" id="openNewProjectForm">
+            <button class="btn btn-primary btn-block" data-toggle="modal" data-target="#newProjectModal" id="addButton">Créer un nouveau projet</button>
+        </div>
+
+        <div class = "col-sm-4">
+            <table class="table table-bordered table-hover" id="TasksList" summary="Table des tâches de l'utilisateur">
+                <tr>
+                    <th class="listTitle text-center" id="taskListTitle">Mes tâches</th>
+                </tr>
+                <tr>
+                    <td>
+                        <span>T3 - Sprint1</span>
+                        <br/>
+                        <span class="subtitle">Nom du projet</span>
+                        <br/>
+                        <span class="subtitle">Du 04/11/2019 au 11/11/2019</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </div>
-<div class = "container-left">
-    <table class="floatLeft" id="projectsList" summary="Table des projets de l'utilisateur">
-        <tr>
-            <th class="listTitle" id="projectListTitle">Mes projets</th>
-        </tr>
-        <?php $i = 0;
-        while($row1 = mysqli_fetch_row($result1)){?>
-            <tr>
-                <td class = tdProject>
-                    <span class="projectTitle"><?php echo $row1[0];?></span>
-                    <br/>
-                    <span class="projectOwner"><?php echo $row1[1];?></span>
-                </td>
-            </tr>
-            <?php
-            $i++;
-        }
-        while ($i<4){
-            echo '<tr><td></td></tr>';
-            $i++;
-        }
-        ?>
-    </table>
-</div>
 
-
-
-<div class = "container-right">
-    <table class="floatRight" id="TasksList" summary="Table des tâches de l'utilisateur">
-        <tr>
-            <th class="listTitle" id="taskListTitle">Mes tâches</th>
-        </tr>
-        <tr>
-            <td>
-                <span>T3 - Sprint1</span>
-                <br/>
-                <span class="subtitle">Nom du projet</span>
-                <br/>
-                <span class="subtitle">Du 04/11/2019 au 11/11/2019</span>
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-        </tr>
-    </table>
-</div>
-<div class="addButton" id="openNewProjectForm">
-    <h1>Nouveau projet</h1>
-    <p class = "subtitle">Créer un nouveau projet</p>
-</div>
-<div id="newProjectModal" class="modal">
-    <div class="modal-content">
-        <form class="form-container" method = "POST">
-            <h1>Nouveau projet</h1>
-
-            <label for="name">Nom du projet :</label>
-            <br/>
-            <input type="text" name="name" required id="projectName">
-            <br/><br/>
-            <label for="description">Description :</label>
-            <br/>
-            <input type="text" name="description" id="projectDescription">
-            <br/><br/>
-            <input type="submit" name="submit" class="submit" value="Créer">
-
-            <button type="button" class="buttonCancel" id="closeForm">Annuler</button>
-        </form>
+<div class="modal" tabindex="-1" role="dialog" id="newProjectModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Nouveau projet</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST">
+                    <div class="form-group">
+                        <label for="projectName">Nom du projet</label>
+                        <input type="text" class="form-control" placeholder="Nom" id="projectName" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="projectDescription">Description</label>
+                        <input type="text" class="form-control" placeholder="Description" id="projectDescription" name="description" required>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" name="submit" class="btn btn-primary">Créer</button>
+                        <button type="button" class="btn btn-secondary buttonCancel" data-dismiss="modal">Annuler</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 
 <?php
-    if(isset($_POST['submit'])){
-        $projectName = $_POST['name'];
-        $projectDesc = $_POST['description'];
-
-        //test que tous les champs sont remplis
-        if(empty($projectName) || empty($projectDesc)){
-            echo "Vous devez remplir tous les champs";
-        }
-        else{
-
-            //test que l'utilisateur n'a pas déjà créé un projet du même nom
-            $sqlTest1 = "SELECT ID_PROJET FROM projet WHERE NOM_PROJET = '$projectName' AND ID_MANAGER = '$userID'";
-            $result1 = mysqli_query($conn,$sqlTest1);
-
-            if(mysqli_num_rows($result1) > 0){
-                echo "Vous avez déjà créé un projet du même nom";
-            }
-            else{
-                $sql = "INSERT INTO projet (NOM_PROJET, ID_MANAGER, DESCRIPTION)
-                VALUES ('$projectName','$userID','$projectDesc')";
-
-                $sql2 = "INSERT INTO membre (ID_MEMBRE, ID_PROJET, NOM_MEMBRE)
-                VALUES ('$userID',LAST_INSERT_ID(),'$userName')";
-
-                if (mysqli_query($conn,$sql) === FALSE) {
-                    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
-                }
-                else if(mysqli_query($conn,$sql2) === FALSE){
-                    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
-                }
-                else{
-                    echo "Votre projet a bien été crée";
-                    header("Refresh:0");
-                }
-            }
-        }
-    }
+echo $message;
 ?>
+
+
 </body>
 </html>
