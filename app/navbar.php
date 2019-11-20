41fd7814106b4c7cb2c6d5240d7cc3e3a24d4fc3
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-11-20 15:09:18 +0100

    fichiers de séparation de la barre de navigation

---
+++ app/navbar.php
@@ -0,0 +1,10 @@
+<link href="../assets/css/navbar.css" rel="stylesheet">
+
+<div class="menuBar">
+    <div class="float-left">
+        <a href="profil.php">GoProject</a>
+    </div>
+    <div class="float-right">
+        <a class="disconnect" href="./index.php">Se déconnecter</a>
+    </div>
+</div>
