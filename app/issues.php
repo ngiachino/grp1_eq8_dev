9d442bdc1564d618dde1e386b65a19e362864510
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-11-20 13:26:43 +0100

    Séparation du PHP et du HTML, et correction de style dans la page des issues

--- app/issues.php
+++ app/issues.php
@@ -1,6 +1,7 @@
 <?php
 
 include '../database/DBconnect.php';
+include 'issuesManagement.php';
 $conn = connect();
 session_start();
 
@@ -13,6 +14,8 @@ if($_SESSION['projectId'] == null){
 }
 
 $idProjet = $_SESSION['projectId'];
+addIssue($conn, $idProjet);
+deleteIssue($conn, $idProjet);
 $query = "SELECT ID_USER_STORY,PRIORITE,DIFFICULTE,DESCRIPTION FROM issue WHERE ID_PROJET = '$idProjet' ORDER BY ID_USER_STORY";
 $result = mysqli_query($conn, $query);
 
@@ -28,14 +31,15 @@ $result = mysqli_query($conn, $query);
 </head>
 
 <body>
-<div class = "menuBar">
-    <div class="menuBar-left">
-        <a id="title" href="profil.php">GoProject</a>
+<div class="menuBar">
+    <div class="float-left">
+        <a href="profil.php">GoProject</a>
     </div>
-    <div class="menuBar-right">
-        <a class="disconnect">Se déconnecter</a>
+    <div class="float-right">
+        <a class="disconnect" href="./index.php">Se déconnecter</a>
     </div>
 </div>
+
 <h1>Issues</h1>
 <form method="POST" id="newIssueForm"></form>
 <form method="POST" id="deleteModifyForm"></form>
@@ -52,56 +56,42 @@ $result = mysqli_query($conn, $query);
   <tbody>
   <?php $i = 1;
     while($row = mysqli_fetch_row($result)){?>
-    <form method = "POST">
     <tr>
-      <th scope="row"><?php echo $i;?></th>
-      <input type="hidden" name="id" value="<?php echo $row[0];?>">
+      <th scope="row"><?php echo $i;?>
+          <input form="deleteModifyForm" type="hidden" name="id" value="<?php echo $row[0];?>">
+      </th>
       <td><?php echo $row[3];?></td>
       <td><?php echo $row[1];?></td>
       <td><?php echo $row[2];?></td>
-      <td><input type="submit" name="modify" value="Modifier"><input type="submit" name="delete" value="Supprimer"></td>
+      <td>
+          <input class="btn btn-info" type="submit" name="modify" value="Modifier">
+          <input class="btn btn-danger" form="deleteModifyForm" type="submit" name="delete" value="Supprimer">
+      </td>
     </tr>
-    </form>
     <?php 
       $i++;
       }
     ?>
     <tr>
         <th scope="row"></th>
-        <td><input type="text" name="description" form="newIssueForm" maxlength="500"></td>
-        <td><input type="text" name="priority" form="newIssueForm" maxlength="10"></td>
-        <td><input type="number" name="difficulty" form="newIssueForm"></td>
-        <td><input type="submit" name="submit" value="Créer" form="newIssueForm"></td> 
+        <td>
+            <input class="form-control" type="text" name="description" form="newIssueForm" maxlength="500" placeholder="Description" required>
+        </td>
+        <td>
+            <select class="form-control" name="priority" form="newIssueForm">
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+            </select>
+        </td>
+        <td>
+            <input class="form-control" type="number" name="difficulty" form="newIssueForm" min="1" step="1" placeholder="Difficulté" required>
+        </td>
+        <td>
+            <input class="btn btn-dark" type="submit" name="submit" value="Créer" form="newIssueForm">
+        </td>
     </tr>
   </tbody>
 </table>
 </body>
 </html>
-<?php
-if(isset($_POST['submit'])){
-  $query = "SELECT MAX(ID_USER_STORY) FROM issue WHERE ID_PROJET='$idProjet'";
-  $result = mysqli_query($conn, $query);
-  if(mysqli_query($conn,$query) === FALSE){
-    //Si il n'y a pas encore d'issue pour ce projet
-    $idUS=1;
-  }
-  else{
-    $idUS = mysqli_fetch_row($result)[0]+1;
-  }
-  $description = $_POST['description'];
-  $priority = $_POST['priority'];
-  $difficulty = $_POST['difficulty'];
-  $query = "INSERT INTO issue (ID_USER_STORY, PRIORITE, DIFFICULTE, DESCRIPTION, ID_PROJET)
-            VALUES ('$idUS','$priority','$difficulty','$description','$idProjet')";
-  if(mysqli_query($conn,$query) === FALSE){
-    echo "Error: " . $query . "<br>" . $conn->connect_error . "<br>";
-  }
-  header("Refresh:0");
-}
-if(isset($_POST['delete'])){
-  $issueID = $_POST['id'];
-  $query = "DELETE FROM issue WHERE ID_USER_STORY = '$issueID' AND ID_PROJET = '$idProjet'";
-  $result = mysqli_query($conn,$query);
-  header("Refresh:0");
-}
-?>
---
+++ app/issuesManagement.php
@@ -0,0 +1,34 @@
+<?php
+function addIssue($conn, $idProjet){
+    if (isset($_POST['submit'])) {
+        $query = "SELECT MAX(ID_USER_STORY) FROM issue WHERE ID_PROJET='$idProjet'";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_query($conn, $query) === FALSE) {
+            //Si il n'y a pas encore d'issue pour ce projet
+            $idUS = 1;
+        } else {
+            $idUS = mysqli_fetch_row($result)[0] + 1;
+        }
+        $description = $_POST['description'];
+        $priority = $_POST['priority'];
+        $difficulty = $_POST['difficulty'];
+        $query = "INSERT INTO issue (ID_USER_STORY, PRIORITE, DIFFICULTE, DESCRIPTION, ID_PROJET)
+            VALUES ('$idUS','$priority','$difficulty','$description','$idProjet')";
+        if (mysqli_query($conn, $query) === FALSE) {
+            echo "Error: " . $query . "<br>" . $conn->connect_error . "<br>";
+        }
+    }
+}
+
+function deleteIssue($conn, $idProjet)
+{
+    if (isset($_POST['delete'])) {
+        $issueID = $_POST['id'];
+        $query = "DELETE FROM issue WHERE ID_USER_STORY = '$issueID' AND ID_PROJET = '$idProjet'";
+        mysqli_query($conn, $query);
+    }
+}
+
+function editIssue($conn){
+
+}
