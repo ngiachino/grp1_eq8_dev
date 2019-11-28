0dba4db2c126bdfe26d1dbf6f20ad1913196d1bb
Amel OULD AMARA <ouldamara.amel@gmail.com>
2019-11-28 22:19:02 +0100

    [US13] Ajout de la fonctionnalité Supprimer une tâche

--- app/management/taskManagement.php
+++ app/management/taskManagement.php
@@ -14,9 +14,10 @@ function addTask($conn,$projectId,$sprintId)
                          VALUES (  '$projectId','$sprintId','1','$description','$duration',
                                     '0','0' )";
 
-        if (!mysqli_query($conn, $query)) {
+        if (mysqli_query($conn, $query) == FALSE) {
             echo "Error: " . $query . "<br>" . $conn->error . "<br>";
-        }
+
+                      }
         else {
                 return " Tâche ajoutée ";
              }
@@ -36,33 +37,32 @@ function assignTask($conn,$projectId, $sprintId)
             return " Veuillez remplir tous les champs!";
         }
         else {
-            $queryExistMember = "SELECT ID_MEMBRE from membre join utilisateur on membre.ID_MEMBRE = utilisateur.ID_USER WHERE NOM_USER = '$userName' ";
+
+            $queryExistMember = "SELECT ID_MEMBRE
+                                 from membre join utilisateur 
+                                             on membre.ID_MEMBRE = utilisateur.ID_USER 
+                                WHERE NOM_USER = '$userName' AND ID_TACHE != '$taskId' ";
+
             $resultMember = mysqli_query($conn, $queryExistMember);
-            if(!($resultMember)){
+            if(!($resultMember))
                  echo  "Error: " . $queryExistMember . "<br>" . $conn->error . "<br>";
-            }
-            if ( mysqli_num_rows($resultMember) == 0)
-            {
-                $queryGet = "SELECT * from utilisateur where NOM_USER ='$userName' ";
-                $resultGetId= mysqli_query($conn, $queryGet);
-                if (!($resultGetId)){
-                    echo "Error Number " .mysqli_num_rows($resultGetId)."RESULT". $queryGet . "<br>" . $conn->error . "<br>";
-                }
-                $row =  mysqli_fetch_row($resultGetId);
+
+            if ( mysqli_num_rows($resultMember) != 0)
+            { // il est membre du projet et du sprint mais cette tâche ne lui est pas encore affectée
+
+                $row =  mysqli_fetch_row($resultMember);
                 $userId= $row[0];
-
-                $queryAssign = " INSERT INTO membre VALUES ('$userId', '$projectId','$userName'
-                           ,'$sprintId', '$taskId')";
-
+                $queryAssign = " INSERT INTO membre 
+                                VALUES ('$userId', '$projectId','$userName'
+                                        ,'$sprintId', '$taskId')";
                 if (!(mysqli_query($conn,$queryAssign)) )
                     {
-                        echo "USERID".$row[0]."Error: ".$row[1] .$queryAssign. "<br>" . $conn->error . "<br>";
+                        echo "Error: ".$queryAssign. "<br>" . $conn->error . "<br>";
                        return "la tâche n'a pas été assignée!";
                     }
 
-            } else{
+            } else
                 return "Cette tâche est déjà assigné à cet utilisateur";
-            }
         }
     }
 }
@@ -73,22 +73,19 @@ function getMemberTask($connexion,$taskId, $sprintId, $projectId)
                         WHERE ID_PROJET = '$projectId' AND ID_SPRINT = '$sprintId' AND ID_TACHE = '$taskId' ";
 
     $result = mysqli_query($connexion, $queryMemberName);
-    if(!$result){
+    if($result == false)
         echo "Error: " . $queryMemberName . "<br>" . $connexion->error . "<br>";
-    }
-     else{
-        return $result;
-     }
+     else return $result;
 }
-//function deleteTask($conn)
-//{
-//    if (isset($_POST['delete'])) {
-//        if (empty($_POST['taskId'])) { return " Impossible";}
-//            $taskId = $_POST['taskId'];
-//            $query = "DELETE FROM Tache WHERE ID_TACHE = '$taskId'";
-//            mysqli_query($conn, $query);
-//        }
-//}
+function deleteTask($conn)
+{
+    if (isset($_POST['delete'])) {
+        if (empty($_POST['taskId'])) { return " Impossible";}
+            $taskId = $_POST['taskId'];
+            $query = "DELETE FROM Tache WHERE ID_TACHE = '$taskId'";
+            mysqli_query($conn, $query);
+        }
+}
 
 
 
--- app/view/tasks.php
+++ app/view/tasks.php
@@ -1,21 +1,190 @@
+<?php
+include  '../management/taskManagement.php';
+include '../../database/DBconnect.php';
+include '../management/sprintManagement.php';
+$conn = connect();
 
+session_start();
+//test si l'utilisateur est connecté. Sinon le renvoie vers l'index
+if($_SESSION['userName'] == null || $_SESSION['userID'] == null){
+    header("Location:index.php");
+}
+//test si l'utilisateur est bien passé par sa page de profil. Sinon le renvoie vers le profil
+else if( $_GET['projectId'] == null || $_GET['sprintId'] == null){
+    header("Location:sprints.php");
+}
+
+$projectId = $_GET['projectId'];
+$sprintId = $_GET['sprintId'];
+$addMessage = addTask($conn,$projectId, $sprintId);
+$assignMessage = assignTask($conn,$projectId, $sprintId );
+$deleteMessage='';
+
+
+$query = "SELECT tache.DESCRIPTION, tache.DUREE_TACHE, tache.IS_DONE, tache.ID_TACHE
+              FROM tache  
+              WHERE tache.ID_PROJET = '$projectId'
+                    AND tache.ID_SPRINT = '$sprintId'
+                    ";
+
+if(mysqli_query($conn, $query) == FALSE)
+    echo "Error: " . $query . "<br>" . $conn->error . "<br>";
+$result = mysqli_query($conn, $query);
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <?php include './defaultHead.php'; ?>
+    <?php include 'defaultHead.php'; ?>
     <title>Tasks - GoProject</title>
     <link href="../../assets/projet.css" rel="stylesheet">
 </head>
 
 <body>
-<?php include 'navbar.php'; ?>
+
+<div class = "menuBar">
+    <div class="menuBar-left">
+        <a id="title" href="profil.php">GoProject</a>
+    </div>
+    <div class="menuBar-right">
+        <a class="disconnect">Se déconnecter</a>
+    </div>
+</div>
 
 <div class="supercontainer">
     <div class="container" id="tasks">
         <h2>Les tâches</h2>
     </div>
 </div>
+
+<div class="container">
+    <!--AFFICHER LE NUMERO DE SPRINT -->
+    <h2>Sprint <?php echo $sprintId ?></h2>
+    <button type="button" class="btn btn-lg  btn-dark" data-toggle="collapse" data-target="#demo">
+        Ajouter une tâche
+    </button>
+
+    <!--CREER UNE TACHE-->
+    <div id="demo" class="collapse">
+        <!-- Le formulaire de creation de la tâche -->
+        <form method="POST">
+            <div class="form-group">
+                <label for="taskDescription">Description de la tâche:</label>
+                <input type="text" class="form-control" name="taskDescription">
+            </div>
+            <div class="form-group">
+                <label for="taskDuration">Durée de la tâche: </label>
+                <input type="text" class="form-control" name="taskDuration">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <!--FIN DU FORMULAIRE-->
+    </div>
+</div>
+<br>
+<br>
+
+<!--AFFICHAGE DES TÂCHES COURRANTES DU SPRINT-->
+<!-- $result recupère les tâches du sprint courant -->
+<!-- tache.DESCRIPTION, tache.DUREE_TACHE, tache.IS_DONE, tache.ID_TACHE -->
+<div class="container">
+    <table class="table" id="taskList" summary="Table des tâches du projet">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Numéro </th>
+            <th scope="col">Description</th>
+            <th scope="col">Durée</th>
+            <th scope="col">Etat</th>
+            <th scope="col">Membre</th>
+
+            <th scope="col">Action</th>
+
+        </tr>
+        </thead>
+
+        <tbody>
+        <?php
+        $i = 1;
+        while($row = mysqli_fetch_row($result)){
+            ?>
+            <tr>
+                <th scope="row"><?php echo $i;?>
+                </th>
+                <td><?php echo $row[0];?></td>
+                <td><?php echo $row[1];?></td>
+                <td><?php
+                    if($row[2] == 1)
+                        echo "DONE";
+                    else{
+                        echo "UNDONE";
+                    }?>
+
+                </td>
+                <td><?php
+                    //LES MEMBRES
+                    $collapseTarget = "#demo".$i;
+                    $target = "demo".$i;
+                    $taskId = $row[3];
+                    $deleteMessage = deleteTask($conn, $taskId);
+                    ?>
+                    <!-- Afficher les membres  de la tâche -->
+
+                    <span>
+                            <?php
+                            $taskId = $row[3];
+                            $resultMember = getMemberTask($conn, $taskId, $sprintId, $projectId);
+                            while($rowMembers = mysqli_fetch_row($resultMember)){
+                                ?>
+                                <ul class="list-group">
+                                    <li class="list-group-item"> <?php echo $rowMembers[0]; ?>
+                                </ul>
+                            <?php  } ?>
+                    </span>
+
+
+
+                </th>
+                </td>
+                <!--Action sur les tâches-->
+                <td>
+
+                    <!--MENU ASSIGNER-->
+                    <button type="button" class="btn  btn-dark" data-toggle="collapse" data-target=<?php echo $collapseTarget;?> >
+                        Assigner la tâche
+                    </button>
+                    <div id=<?php echo $target; ?> class="collapse">
+                        <!-- Le formulaire d'attribution de la tâche -->
+                        <form method="POST">
+                            <input type="hidden" name="taskId" value=<?php echo $row[3];?>>
+                            <div class="form-group">
+                                <label for="userName">User:</label>
+                                <input type="text" class="form-control" name="userName">
+                            </div>
+                            <button type="submit" name="assigner" class="btn btn-primary">Assigner</button>
+                        </form>
+                    </div>
+                    <!--FIN Des ACTIONS FORMULAIRE-->
+                    <!--BOUTON DELETE-->
+                    <br><br>
+                    <form method="POST">
+
+                        <button type="submit" name="deleteTask" class="btn btn-secondary">Supprimer</button>
+                    </form>
+                </td>
+            </tr>
+            <?php
+            $i++;
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+<?php
+echo $addMessage;
+echo $assignMessage;
+echo $deleteMessage;
+$connexion=null;
+?>
 
 </body>
 </html>
