58ead59746e1d23f36707a6b1034234fa10bf30a
Amel OULD AMARA <ouldamara.amel@gmail.com>
2019-12-05 00:33:12 +0100

    [US11] Correction de l'affichage des membres/issues + style de la page

--- src/app/view/tasks.php
+++ src/app/view/tasks.php
@@ -29,6 +29,7 @@ $query = "SELECT DISTINCT tache.DESCRIPTION, tache.DUREE_TACHE, tache.IS_DONE, t
               WHERE tache.ID_PROJET = '$projectId'
                     AND tache.ID_SPRINT = '$sprintId'
                     ";
+
 $result = mysqli_query($conn, $query);
 ?>
 <!DOCTYPE html>
@@ -41,7 +42,7 @@ $result = mysqli_query($conn, $query);
 </head>
 <body>
 <?php include 'navbar.php'; ?>
-<div class="container-fluid">
+<div class="container">
     <!--AFFICHER LE NUMERO DE SPRINT -->
     <div class="row">
         <div class="col-md-3">
@@ -59,6 +60,7 @@ $result = mysqli_query($conn, $query);
                 <label for="taskDescription">Description de la tâche:</label>
                 <input type="text" class="form-control" id="taskDescription" name="taskDescription"/>
             </div>
+
             <div class="form-group">
                 <label for="taskDuration">Durée de la tâche: </label>
                 <input type="text" class="form-control" id="taskDuration" name="taskDuration">
@@ -72,7 +74,7 @@ $result = mysqli_query($conn, $query);
 <br>
 <br>
 <!--AFFICHAGE DES TÂCHES COURRANTES DU SPRINT-->
-<div class="container-fluid">
+<div class="container">
     <table class="table" id="taskList">
         <thead class="thead-dark">
         <tr>
@@ -96,18 +98,17 @@ $result = mysqli_query($conn, $query);
                 <td><?php echo $row[1];?></td>
                 <!--ETAT-->
                 <td>
+                    <!-- EDIT FORM DE L ETAT-->
+                    <i  class="fas fa-edit float-left" data-toggle="collapse" data-target="<?php echo "#state".$i;?>">
+                    </i>
                     <?php
                     echo $row[2];
                     ?>
-                    <!--<i class="fas fa-edit"></i>-->
-                    <i  class="fas fa-edit" data-toggle="collapse" data-target="<?php echo "#state".$i;?>">
-                    </i>
-                    <br>
                     <div id=<?php echo "state".$i; ?> class="collapse">
                         <form method="POST">
                             <input type="hidden" name="taskId" value=<?php echo $row[3];?>>
                             <div class="form-group">
-                                <label for="taskState"></label>
+                                <label for="taskState">Nouvel Etat :</label>
                                 <br>
                                 <select class="form-control-sm" name="taskState" id="taskState" >
                                     <option value="TO DO">TO DO</option>
@@ -118,35 +119,81 @@ $result = mysqli_query($conn, $query);
                             <button type="submit" name="editTaskState" class="btn btn-secondary btn-sm">Valider</button>
                         </form>
                     </div>
+                    <!--AFFICHAGE DE L ETAT-->
+                    <div>
+
+                    </div>
                 </td>
                 <!--MEMBRES-->
-                <td>
-                    <ul>
-                        <?php
-                        //LES MEMBRES
-                        $taskId = $row[3];
-                        startDeleteMember($conn, $projectId, $sprintId, $taskId);
-                        $taskMembers = getMemberTask($conn, $taskId, $sprintId, $projectId);
-
-                        // Afficher les membres  de la tâche
-                        $j = 0;
-                        while ($member = mysqli_fetch_row($taskMembers)) {
-                            $memberName = $member[0];
-                            ?>
-                            <li class="d-flex">
-                                <form method="post">
-                                    <?php echo '- '.$member[0]; ?>
-                                    <input class="form-control" type="hidden" name="idMember" value="<?php echo $member[1];?>">
-                                    <button type="submit" class="fa fa-times btn btn-danger" name="deleteMember"></button>
+                <td >
+                    <!-- ADD MEMBER FORM-->
+                            <i class="fas fa-user-plus float-left" data-toggle="collapse" data-target="<?php echo "#demo".$i;?>"> </i>
+                            <div id="<?php echo "demo".$i; ?>" class="collapse float-center">
+                                <!-- Le formulaire d'attribution de la tâche -->
+                                <br>
+                                <form method="POST">
+                                    <input type="hidden" name="taskId" value=<?php echo $row[3];?>>
+                                    <div class="form-group">
+                                        <label for="userName">User:</label>
+                                        <br>
+                                        <input type="text" class="form-control-sm" id="userName" name="userName">
+                                    </div>
+                                    <button type="submit" name="assigner" class="btn btn-secondary btn-sm float-left" >Assigner</button>
                                 </form>
-                            </li>
-                            <?php $j++;
-                        } ?>
-                    </ul>
+                            </div>
+                    <br>
+                    <!--LISTE DES MEMBRES-->
+                    <table class="table table-no-border">
+                        <tbody>
+                            <!-- LIST OF TASK'S MEMBERS-->
+                                <br>
+                                <?php
+                                //LES MEMBRES
+                                $taskId = $row[3];
+                                startDeleteMember($conn, $projectId, $sprintId, $taskId);
+                                $taskMembers = getMemberTask($conn, $taskId, $sprintId, $projectId);
+
+                                // Afficher les membres  de la tâche
+                                $j = 0;
+                                while ($member = mysqli_fetch_row($taskMembers)) {
+                                    $memberName = $member[0];
+                                    ?>
+                                <tr>
+                                    <td >
+                                        <form method="post">
+                                            <?php echo $member[0]; ?>
+                                              <button type="submit" class="fas fa-times btn bg-transparent btn-sm float-left" style="color:red;" name="deleteMember"></button>
+                                            <input class="form-control" type="hidden" name="idMember" value="<?php echo $member[1];?>">
+                                        </form>
+                                    </td>
+                                    <?php $j++;
+                                } ?>
+                          </tr>
+                        </tbody>
+                      </table>
                 </td>
                 <!--USER STORIES-->
                 <td>
-                    <ul>
+                    <i class="fas fa-plus float-left " data-toggle="collapse" data-target="<?php echo "#us".$i;?>">
+                    </i>
+                    <div id=<?php echo "us".$i; ?> class="collapse">
+                        <!-- Le formulaire d'ajout d'une US -->
+                        <br>
+                        <form method="POST">
+                            <div class="form-group">
+                                <input type="hidden" name="taskIdentificateur" value=<?php echo $row[3];?>>
+
+                                <label for="issueId">User Story:</label>
+                                    <br>
+                                <input type="text" class="form-control-sm" id="issueId" name="issueId">
+                            </div>
+                            <button type="submit" name="lier" class="btn btn-secondary btn-sm float-left">Lier</button>
+                        </form>
+                    </div>
+                    <br><br>
+                     <table class="table table-no-border">
+                        <tbody>
+
                         <?php
                         deleteIssueFromTask($conn,$projectId);
                         $resultIssues = getIssuesTask($conn, $taskId, $projectId);
@@ -154,77 +201,51 @@ $result = mysqli_query($conn, $query);
                         $issueId = $rowIssue[0];
                         $issueDescription = $rowIssue[1];
                         ?>
+                        <tr>
                         <!--Bouton delete-->
-
-                        <li class="list-group-item">
+                            <td>
                             <form method="POST">
+                                <button type="submit" class="fas fa-times btn bg-transparent btn-sm float-left" style="color:red;" name="deleteIssue"></button>
                                 <?php echo $issueId.'-'.$issueDescription; ?>
                                 <input type="hidden" name="issueId" value="<?php echo $issueId;?>">
                                 <input type="hidden" name="taskId" value="<?php echo $taskId;?>">
-                                <button type="submit" class="fa fa-trash" name="deleteIssue"></button>
+
                             </form>
-                        </li>
-                    </ul>
+                        </td>
+                    </tr>
                     <?php } ?>
+                    </tbody>
+                </table>
                 </td>
                 <!--ACTION-->
                 <td>
-                    <!--MENU ASSIGNER-->
-                    <button type="button" class="btn btn-dark" data-toggle="collapse" data-target="<?php echo "#demo".$i;?>">
-                        Assigner la tâche
-                    </button>
-                    <div id=<?php echo "demo".$i; ?> class="collapse">
-                        <!-- Le formulaire d'attribution de la tâche -->
-                        <form method="POST">
-                            <input type="hidden" name="taskId" value=<?php echo $row[3];?>>
-                            <div class="form-group">
-                                <label for="userName">User:</label>
-                                <input type="text" class="form-control" id="userName" name="userName">
-                            </div>
-                            <button type="submit" name="assigner" class="btn btn-primary">Assigner</button>
-                        </form>
-                    </div>
-                    <br> <br>
-                    <!-- AJOUTER -->
-                    <button type="button" class="btn btn-dark" data-toggle="collapse" data-target="<?php echo "#us".$i;?>">
-                        Ajouter une USS
-                    </button>
-                    <div id=<?php echo "us".$i; ?> class="collapse">
-                        <!-- Le formulaire d'ajout d'une US -->
-                        <form method="POST">
-                            <div class="form-group">
-                                <input type="hidden" name="taskIdentificateur" value=<?php echo $row[3];?>>
-                                <label for="issueId">User Story:</label>
-                                <input type="text" class="form-control" id="issueId" name="issueId">
-                            </div>
-                            <button type="submit" name="lier" class="btn btn-primary">Lier</button>
-                        </form>
-                    </div>
+
+                    <!-- ACTION SUR UNE TACHE-->
+                     <!-- DELETE-->
+                    <form method="POST">
+                        <input type="hidden" name="taskId" value="<?php echo $row[3];?>">
+                        <button type="submit" name="delete" class="fa fa-close btn btn-light "></button>
+                    </form>
                     <br>
                     <!--MODIFIER UNE TÂCHE  -->
-                    <button type="button" class="btn  btn-dark" data-toggle="collapse" data-target=<?php echo "#modifier".$i;?> >
-                        Modifier la tâche
+                    <button class="fas fa-edit btn btn-light float-left " data-toggle="collapse" data-target=<?php echo "#modifier".$i;?> >
                     </button>
                     <div id=<?php echo "modifier".$i; ?> class="collapse">
                         <!-- Le formulaire d'attribution de la tâche -->
-                        <form method="POST">
+                        <br><br>
+                            <form method="POST">
                             <div class="form-group">
-                                <input type="hidden" name="taskId" value="<?php echo $row[3];?>">
-                                <label for="descriptionTask">Description:</label>
-                                    <input type="text" class="form-control" id="descriptionTask" name="descriptionTask" placeholder=<?php echo $row[0];?>>
+                                <label for="descriptionTask">Description:</label> <br>
+                                    <input type="text" class="form-control-sm" id="descriptionTask" name="descriptionTask" placeholder=<?php echo $row[0];?>>
                                 <br>
-                                <label for="durationTask">Durée:</label>
-                                   <input type="text" class="form-control" id="durationTask" name="durationTask" placeholder=<?php echo $row[1];?>>
+                                <label for="durationTask">Durée:</label> <br>
+                                   <input type="text" class="form-control-sm" id="durationTask" name="durationTask" placeholder=<?php echo $row[1];?>>
+                                <input type="hidden" name="taskId" value="<?php echo $row[3];?>">
                             </div>
-                            <button type="submit" name="modifier" class="btn btn-primary">Modifier</button>
+                            <button type="submit" name="modifier" class="btn btn-secondary btn-sm">Modifier</button>
                         </form>
                     </div>
-                    <!-- DELETE-->
-                    <br><br>
-                    <form method="POST">
-                        <input type="hidden" name="taskId" value="<?php echo $row[3];?>">
-                        <button type="submit" name="delete" class="btn btn-secondary">Supprimer</button>
-                    </form>
+
                     <!--FIN Des ACTIONS FORMULAIRE-->
                 </td>
             </tr>
@@ -241,3 +262,4 @@ $connexion=null;
 
 </body>
 </html>
+
