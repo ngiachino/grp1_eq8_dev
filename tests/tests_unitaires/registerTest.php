429c0c2c6e5bb2b4890b0d5e87cba1f20ee35844
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-12-05 00:25:48 +0100

    [TESTS] Correction de registerTest

--- tests/tests_unitaires/registerTest.php
+++ tests/tests_unitaires/registerTest.php
@@ -1,46 +1,46 @@
-<?php
-    include_once 'src/database/DBconnect.php';
-    include_once 'src/app/management/registerManagement.php';
-    use PHPUnit\Framework\TestCase;
-    /**
-    * @group testsUnitaires
-    *
-    */
-    class RegisterTest extends TestCase{
-        /** @test */
-        public function testRegister(){
-            $conn = connect();
-            $this->clear();
+ass <?php
+include_once 'src/database/DBconnect.php';
+include_once 'src/app/management/registerManagement.php';
+use PHPUnit\Framework\TestCase;
+/**
+ * @group testsUnitaires
+ *
+ */
+class RegisterTest extends TestCase{
+    /** @test */
+    public function testRegister(){
+        $conn = connect();
+        $this->clear();
 
-            $res = register("TestAccount","TestAccount@test.fr","test","test");
-            $this->assertEquals($res,"Votre compte a bien été créé");
-            $sql = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = 'TestAccount'";
-            $result = $conn->query($sql);
-            $this->assertEquals($result->num_rows, 1);
+        register("TestAccount","TestAccount@test.fr","test","test");
+        $this->assertContains("Votre compte a bien été créé", getMessage());
+        $sql = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = 'TestAccount'";
+        $result = $conn->query($sql);
+        $this->assertEquals($result->num_rows, 1);
 
-            $res = register("TestAccount","Test@test.test","test2","test2");
-            $this->assertEquals($res,"Ce pseudo est déjà associé à un compte");
-            $sql = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = 'TestAccount'";
-            $result = $conn->query($sql);
-            $this->assertEquals($result->num_rows, 1);
+        register("TestAccount","Test@test.test","test2","test2");
+        $this->assertContains("Ce pseudo est déjà associé à un compte", getMessage());
+        $sql = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = 'TestAccount'";
+        $result = $conn->query($sql);
+        $this->assertEquals($result->num_rows, 1);
 
-            $res = register("Test","TestAccount@test.fr","test2","test2");
-            $this->assertEquals($res,"Ce mail est déjà associé à un compte");
-            $sql = "SELECT ID_USER FROM utilisateur WHERE MAIL_USER = 'TestAccount@test.fr'";
-            $result = $conn->query($sql);
-            $this->assertEquals($result->num_rows, 1);
+        register("Test","TestAccount@test.fr","test2","test2");
+        $this->assertContains("Ce mail est déjà associé à un compte", getMessage());
+        $sql = "SELECT ID_USER FROM utilisateur WHERE MAIL_USER = 'TestAccount@test.fr'";
+        $result = $conn->query($sql);
+        $this->assertEquals($result->num_rows, 1);
 
-            $res = register("TestAccount2","TestAccount2@test.fr","test","test2");
-            $this->assertEquals($res,"Les mots de passe ne sont pas identiques");
-            $sql = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = 'TestAccount2'";
-            $result = $conn->query($sql);
-            $this->assertEquals($result->num_rows, 0);
+        register("TestAccount2","TestAccount2@test.fr","test","test2");
+        $this->assertContains("Les mots de passe ne sont pas identiques", getMessage());
+        $sql = "SELECT ID_USER FROM utilisateur WHERE NOM_USER = 'TestAccount2'";
+        $result = $conn->query($sql);
+        $this->assertEquals($result->num_rows, 0);
 
-            $this->clear();
-        }
-        private function clear(){
-            $conn = connect();
-            $sql = "DELETE FROM utilisateur WHERE NOM_USER = 'TestAccount' OR MAIL_USER = 'TestAccount@test.fr' OR NOM_USER = 'TestAccount2' OR MAIL_USER = 'TestAccount@test.fr'";
-            $conn->query($sql);
-        }
+        $this->clear();
     }
+    private function clear(){
+        $conn = connect();
+        $sql = "DELETE FROM utilisateur WHERE NOM_USER = 'TestAccount' OR MAIL_USER = 'TestAccount@test.fr' OR NOM_USER = 'TestAccount2' OR MAIL_USER = 'TestAccount@test.fr'";
+        $conn->query($sql);
+    }
+}
