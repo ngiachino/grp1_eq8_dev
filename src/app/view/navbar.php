5e1bebbcb51d38737a0757b9da2aabdf86b75d8b
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-12-09 04:02:52 +0100

    [US4] Mise en place d'un bouton permettant de revenir au dashboard d'un projet

--- src/app/view/navbar.php
+++ src/app/view/navbar.php
@@ -1,8 +1,16 @@
+<?php
+include_once '../management/projectManagement.php';
+$row = mysqli_fetch_row(startProfil());
+?>
+
 <link href="../../assets/css/navbar.css" rel="stylesheet">
 
 <div class="menuBar">
     <div class="float-left">
         <a href="profil.php">GoProject</a>
+    </div>
+    <div class="float-left px-4">
+        <a href="./projet.php?title=<?php echo $row[0];?>&owner=<?php echo $row[1];?>">Projet</a>
     </div>
     <div class="float-right">
         <a class="disconnect" href="../../index.php">Se déconnecter</a>
